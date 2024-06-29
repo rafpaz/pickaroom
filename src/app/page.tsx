75416ed863ad295104c9b75da9b6931daa39066f
Rafael <refaelypaz@gmail.com>
2024-06-29 21:08:06 +0200

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { notFound } from "next/navigation";
+import { asText } from "@prismicio/client";
+import { SliceZone } from "@prismicio/react";
+
+import { createClient } from "@/prismicio";
+import { components } from "@/slices";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+  const page = await client.getByUID("page", "home").catch(() => notFound());
+
+  return {
+    title: asText(page.data.title),
+    description: page.data.meta_description,
+    openGraph: {
+      title: page.data.meta_title ?? undefined,
+      images: [{ url: page.data.meta_image.url ?? "" }],
+    },
+  };
+}
+
+export default async function Page() {
+  const client = createClient();
+  const page = await client.getByUID("page", "home").catch(() => notFound());
+  const bla = await client.getByUID("homepage", "123");
+
+  return <SliceZone slices={page.data.slices} components={components} />;
+}
