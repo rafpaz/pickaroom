36f94fae0f41cd0ddd5a505b6b94a2c75a7a8c32
Rafael <refaelypaz@gmail.com>
2024-06-29 21:17:20 +0200

    oops

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,6 @@ export async function generateMetadata(): Promise<Metadata> {
 export default async function Page() {
   const client = createClient();
   const page = await client.getByUID("page", "home").catch(() => notFound());
-  const bla = await client.getByUID("homepage", "123");
 
   return <SliceZone slices={page.data.slices} components={components} />;
 }
