af7d4f287f30dcd1457a8db5ff1c1ef63b3abf3d
rafa-cos <rafael.paz@caronsale.de>
2025-03-05 11:00:12 +0100

    italic, grid animation

--- src/slices/Grid/index.tsx
+++ src/slices/Grid/index.tsx
@@ -21,9 +21,7 @@ const Grid = ({ slice }: GridProps) => {
       data-slice-variation={slice.variation}
     >
       <Bounded yPadding="sm" className="relative">
-        <CustomAnimation className="mb-16">
-          <PrismicRichText field={slice.primary.title} />
-        </CustomAnimation>
+        <PrismicRichText field={slice.primary.title} />
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
           {slice.primary.items.map((item, index) => (
             <OptionalPrismicNextLink key={index} field={item.link}>
